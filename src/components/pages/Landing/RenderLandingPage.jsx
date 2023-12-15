209f74c6bc52aa8c2571a791056b8efd87330a81
Matt Phillips <mattcphillips17@gmail.com>
2023-12-14 20:36:32 -0800

    completed landing page

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -58,6 +58,15 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+
+        <a href="https://humanrightsfirst.org/wp-content/uploads/2022/10/COW2021001887-I589Data.csv">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          >
+            Download the Data
+          </Button>
+        </a>
       </div>
 
       <div className="middle-section">
@@ -76,12 +85,59 @@ function RenderLandingPage(props) {
           </h3>
         </div>
       </div>
-      <div>
-        {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
-        <p onClick={() => scrollToTop()} className="back-to-top">
-          Back To Top ^
-        </p>
+
+      {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+      <div className="bottom-section">
+        <h2 className="bottom-section-title">Systemic Disparity Insights</h2>
+
+        <div className="data-point-container">
+          <div className="data-point">
+            <h3>36%</h3>
+            <p>
+              By the end of the Trump administration, the average asylum office
+              grant rate had fallen 36 percent from an average of 44 percent in
+              fiscal year 2016 to 28 percent in fiscal year 2020.
+            </p>
+          </div>
+          <div className="data-point">
+            <h3>5%</h3>
+            <p>
+              The New York asylum office grant rate dropped to 5 percent in
+              fiscal year 2020.
+            </p>
+          </div>
+          <div className="data-point">
+            <h3>6x Lower</h3>
+            <p>
+              Between fiscal year 2017 and 2020, the New York asylum office’s
+              average grant rate was six times lower than the San Francisco
+              asylum office.
+            </p>
+          </div>
+        </div>
+
+        <div className="read-more-btn">
+          <a href="https://humanrightsfirst.org/library/uscis-records-reveal-systemic-disparities-in-asylum-decisions/">
+            <Button
+              type="default"
+              style={{
+                backgroundColor: '#404C4A',
+                color: '#FFFFFF',
+                fontFamily: 'Segoe Ui',
+              }}
+              onClick={() => history.push('/graphs')}
+            >
+              Read More
+            </Button>
+          </a>
+        </div>
+
+        <div className="back-to-top-container">
+          <div className="back-to-top" onClick={() => scrollToTop()}>
+            <p>Back To Top</p>
+            <p>^</p>
+          </div>
+        </div>
       </div>
     </div>
   );
